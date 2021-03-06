4179e8e3ed5d8d7d249f283b57b9eba84a3b4780
kutayk33 <kutaykutlu33@gmail.com>
2021-03-06 21:17:26 +0100

    .

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 ################# LIBRARIES #############
-from keras.preprocessing.text import Tokenizer
-#from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import azure.cognitiveservices.speech as speechsdk
 from google_trans_new import google_translator
